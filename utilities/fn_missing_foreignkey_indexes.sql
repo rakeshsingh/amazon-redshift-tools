1f7a6cd2834358a55a39f8363aa29ee8ff0edfe8
Justin Leto <leto.justin@gmail.com>
2013-06-23 16:54:31 -0400

    Added create_indexes boolean parameter to allow user to report back missing indexes and display statements to be executed before creating indexes.
    Renamed the file from fn_create_missing_foreignkey_indexes.sql to fn_missing_foreignkey_indexes.sql

--- utilities/fn_missing_foreignkey_indexes.sql
+++ utilities/fn_missing_foreignkey_indexes.sql
@@ -2,17 +2,33 @@
 * @author: Justin Leto
 * @date: June 15, 2013
 *
+* function: _utility.fn_missing_foreignkey_indexes(create_indexes boolean)
+*
 * description:
 * 	Foreign key columns are a special case that demands they have indexes.
-* 	This function searches all foreign keys in the current schema and 
-* 	creates indexes on all foreign keys columns.
+* 	This function searches all foreign keys in the current schema and allows the user to
+*	control, through the create_indexes parameter, whether the function creates or reports
+*	the missing indexes on foreign key indexes.
 *
-*usage:
-*	do $$
-*	begin
-*		perform _utility.fn_create_missing_foreignkey_indexes();
-*	end $$;
+* parameters:
+*	create_indexes (boolean): Flag to control whether missing indexes are created or just reported.
+*							  True - Create the missing indexes.
+*							  False - Report the missing indexes but do not create.
 *
+* usage:
+*/
+/*
+	--reporting
+	do $$
+	begin
+		perform _utility.fn_missing_foreignkey_indexes(false);
+	end $$;
+	
+	--creating indexes:
+	do $$
+	begin
+		perform _utility.fn_missing_foreignkey_indexes(true);
+	end $$;
 */
 
 /* Check for _utility schema. If it doesn't exist, create it. */
@@ -24,7 +40,7 @@ begin
 	end if;
 end $$;
 
-create or replace function _utility.fn_create_missing_foreignkey_indexes()
+create or replace function _utility.fn_missing_foreignkey_indexes(create_indexes boolean)
 returns boolean as
 $BODY$
 declare create_index_sql text = '';
@@ -102,17 +118,21 @@ begin
 		/* Test if missing foreign key indexes were found */
 		if missing_index_count > 0
 		then
-			raise notice E'% missing foreign key indexes will be created: \n%', missing_index_count, create_index_sql;
-			/* Create missing foreign key indexes */
-			execute create_index_sql;
-			raise notice 'Indexes created successfully.';	
+			if create_indexes then
+				raise notice E'\n% missing foreign key indexes will be created: \n%', missing_index_count, create_index_sql;
+				/* Create missing foreign key indexes */
+				execute create_index_sql;
+				raise notice E'\nIndexes created successfully.';
+			else
+				raise notice E'\nThere are % missing foreign key indexes.\nRun this function with a create_indexes paramter set to true to execute the following statements: \n%', missing_index_count, create_index_sql;			
+			end if;
 		else
-			raise notice 'There are no missing foreign key indexes.';
+			raise notice E'\nThere are no missing foreign key indexes.';
 		end if;
 
 		/* Catch and raise notice of exceptions */
 		exception when others then
-			raise notice '% %', SQLERRM, SQLSTATE;
+			raise notice E'\n% %', SQLERRM, SQLSTATE;
 			/* Report failure */
 			return false;
 	end;
