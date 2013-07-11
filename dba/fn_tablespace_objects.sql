eb344190c03fc0f64457f99fb5e0792e7297531b
Justin Leto <leto.justin@gmail.com>
2013-07-11 18:02:29 -0400

    Added _dba.fn_tablespace_parentless_indexes to report indexes that are in different tablespaces than their parent tables. This is helpful after doing table migrations to different tablespaces. 	modified:   fn_tablespace_objects.sql

--- dba/fn_tablespace_objects.sql
+++ dba/fn_tablespace_objects.sql
@@ -3,13 +3,14 @@
 * @date: July 8th, 2013
 *
 * Description:
-* 	Function to list all tablespaces and their corresponding objects.
+* 	DBA functions that report various information about tablespaces.
 *
 */
 
 /* usage:
 
 	select * from _dba.fn_tablespace_objects()
+	select * from _dba.fn_tablespace_parentless_indexes()
 
 */
 
@@ -51,4 +52,34 @@ begin
 	and relnamespace not in (select oid from pg_namespace where nspname in ('pg_catalog', 'information_schema'));
 end; $BODY$ language plpgsql;
 
-comment on function _dba.fn_tablespace_objects() is 'DBA script to view tablespaces and their corresponding objects.';
+comment on function _dba.fn_tablespace_objects() is 'DBA function to view tablespaces and their corresponding objects.';
+
+/* Function to view indexes in different tablespaces than their parent objects */
+create or replace function _dba.fn_tablespace_parentless_indexes()
+returns table (
+	indexname name,
+	index_tablespace name,
+	tablename name,
+	table_tablespace name
+	)
+as $BODY$
+begin
+	return query
+	select i.relname as indexname,
+			tsi.spcname as index_tablespace,
+			t.relname as tablename,
+			tst.spcname as table_tablespace
+	from (((pg_class t
+			join pg_tablespace tst
+				on t.reltablespace = tst.oid)
+			join pg_index pgi
+				on pgi.indexrelid = t.oid)
+			join pg_class i
+				on pgi.indexrelid = i.oid)
+			join pg_tablespace tsi
+				on i.reltablespace = tsi.oid
+	where i.relname not like 'pg_toast%'
+	and	i.reltablespace != t.reltablespace;
+end; $BODY$ language plpgsql;
+
+comment on function _dba.fn_tablespace_parentless_indexes() is 'DBA function view indexes in different tablespaces than their parent objects.';
