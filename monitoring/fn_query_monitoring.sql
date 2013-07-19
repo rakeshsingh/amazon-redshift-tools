c9b9ef497ced2152618a01db3540bebd8a94f51e
Justin Leto <leto.justin@gmail.com>
2013-07-19 05:54:37 -0400

    Added functions to monitor queries being executed on the server and to report activity of tables.

---
+++ monitoring/fn_query_monitoring.sql
@@ -0,0 +1,106 @@
+﻿/*
+* @author: Justin Leto
+* @date: July 18, 2013
+*
+* description:
+* 	Query monitoring functions
+*/
+
+/* usage:
+		select * from _monitoring.fn_connected_clients();
+		select * from _monitoring.fn_users_blocking_queries();
+		select * from _monitoring.fn_waiting_queries();
+*/
+
+/* check if the _monitoring schema exists */
+do $$
+begin
+	if not (select exists(select * from information_schema.schemata where schema_name = '_monitoring'))
+	then
+		create Schema _monitoring;
+	end if;
+end $$;
+
+/* Function lists queries being blocked and which users are blocking them. */
+create or replace function _monitoring.fn_users_blocking_queries()
+returns table (
+	waiting_query text,
+	waiting_pid int,
+	waiting_user name,
+	locking_query text,
+	locking_pid int,
+	locking_user name,
+	tablename text
+) as $BODY$
+begin
+	return query
+	select w.current_query as waiting_query,
+		   w.procpid as waiting_pid,
+		   w.usename as waiting_user,
+		   l.current_query as locking_query,
+		   l.procpid as locking_pid,
+		   l.usename as locking_user,
+		   t.schemaname || '.' || t.relname as tablename
+	from pg_stat_activity w
+	join pg_locks l1 on w.procpid = l1.pid and not l1.granted
+	join pg_locks l2 on l1.relation = l2.relation and l2.granted
+	join pg_stat_activity l on l2.pid = l.procpid
+	join pg_stat_user_tables t on l1.relation = t.relid
+	where w.waiting;
+end; $BODY$ language plpgsql security definer;
+
+comment on function _monitoring.fn_users_blocking_queries() is 'Lists queries being blocked and which users are blocking them.';
+
+/* Function lists queries run by all users. */
+create or replace function _monitoring.fn_active_queries()
+returns table (
+	db_name name,
+	user_name name,
+	current_query text
+) as $BODY$
+begin
+	return query
+	select datname as db_name,
+		   usename as user_name,
+		   current_query
+	from pg_stat_activity
+	where current_query != '<IDLE>';	
+end; $BODY$ language plpgsql security definer;
+
+/* Function to view connected clients */
+create or replace function _monitoring.fn_connected_clients()
+returns table (
+	db_name name,
+	user_name name,
+	client_ip inet,
+	client_port int
+) as $BODY$
+begin
+	return query
+	select act.datname,
+		   act.usename,
+		   act.client_addr,
+		   act.client_port
+	from pg_stat_activity act;
+end; $BODY$ language plpgsql security definer;
+
+comment on function _monitoring.fn_active_queries() is 'Lists queries run by all users.';
+
+/* Function to show queries waiting on locks. */
+create or replace function _monitoring.fn_waiting_queries()
+returns table (
+	db_name name,
+	user_name name,
+	current_query text
+) as $BODY$
+begin
+	return query
+	select act.datname as db_name,
+		   act.usename as user_name,
+		   act.current_query
+	from pg_stat_activity act
+	where waiting;
+end; $BODY$ language plpgsql security definer;
+
+comment on function _monitoring.fn_waiting_queries() is 'Show queries waiting on locks.';
+
---
+++ monitoring/fn_table_activity_monitoring.sql
@@ -0,0 +1,89 @@
+﻿/*
+* @author: Justin Leto
+* @date: July 19, 2013
+*
+* description:
+*   Table activity monitoring functions
+*/
+
+/* usage:
+		select * from _monitoring.fn_table_activity()
+	
+*/
+
+/* check if the _monitoring schema exists */
+do $$
+begin
+	if not (select exists(select * from information_schema.schemata where schema_name = '_monitoring'))
+	then
+		create Schema _monitoring;
+	end if;
+end $$;
+
+drop type if exists _monitoring.fileinfo cascade;
+
+/* Type to support table_file_info function */
+create type _monitoring.fileinfo as (
+	file_name text,
+	file_size bigint,
+	ctime abstime,
+	mtime abstime,
+	atime abstime
+);
+
+comment on type _monitoring.fileinfo is 'Type to support _monitoring.fn_table_activity';
+
+/* Function to show last table activity by querying table files. */
+create or replace function _monitoring.fn_table_activity(schemaname text, tablename text)
+returns setof _monitoring.fileinfo
+as $BODY$
+    import datetime, glob, os
+    db_info = plpy.execute("""
+                           select datname as db_name,
+                           current_setting('data_directory') || '/base/' || db.oid as data_directory
+                           from pg_database db
+                           where datname = current_database()
+                           """)
+    #return db_info[0]['data_directory']
+    table_info_plan = plpy.prepare("""
+    select nspname as schemaname,
+           relname as tablename,
+           relfilenode as filename
+     from pg_class c
+     join pg_namespace ns on c.relnamespace=ns.oid
+    where nspname=$1
+      and relname=$2
+    """, ['text','text'])
+    table_info = plpy.execute(table_info_plan,[schemaname,tablename])
+    filemask = '%s/%s*' % (db_info[0]['data_directory'], table_info[0] ['filename'])
+    res=[]
+    for filename in glob.glob(filemask):
+        fstat = os.stat(filename)
+        res.append((filename, fstat.st_size, datetime.datetime.fromtimestamp(fstat.st_ctime).isoformat(), datetime.datetime.fromtimestamp(fstat.st_mtime).isoformat(), datetime.datetime.fromtimestamp(fstat.st_atime).isoformat()))
+    return res
+$BODY$ language plpythonu;
+
+comment on function _monitoring.fn_table_activity(schemaname text, tablename text) is 'Function to show last table activity by querying table files.';
+
+
+/* Function to report table activity - latest modification and read access.*/
+create or replace function _monitoring.fn_table_activity()
+returns table (
+	table_schema text,
+	table_name text,
+	latest_mod abstime,
+	latest_read abstime
+) as $$
+begin
+	return query
+	select t.table_schema::text,
+	t.table_name::text,
+	(select max(mtime) as latest_mod from (select * from _monitoring.fn_active_tables(t.table_schema, t.table_name)) table_file) as latest_mod,
+	(select max(atime) as latest_read from (select * from _monitoring.fn_active_tables(t.table_schema, t.table_name)) table_file) as latest_read
+	from information_schema.tables t
+	where t.table_schema not in ('pg_catalog', 'information_schema')
+	and t.table_type = 'BASE TABLE';
+end; $$ language plpgsql;
+
+comment on function _monitoring.fn_table_activity() is 'Function to report table activity - latest modification and read access.';
+
